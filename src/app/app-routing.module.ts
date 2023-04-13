ad37d753efc322e551c5cead077a2178cd2a9a23
FrankAyalaSol <201810595@urp.edu.pe>
2023-04-13 16:29:53 -0500

    SeguricomV1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Componentes
+import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { CrearUsuarioComponent } from './components/crear-usuario/crear-usuario.component';
+import { E404Component } from './components/e404/e404.component';
+
+const routes: Routes = [
+  {
+    path: '',
+     component:PrincipalComponent
+  },
+  {
+    path: 'Iniciar_Sesion',
+     component:IniciarSesionComponent
+  },
+  {
+    path: 'Crear_Usuario',
+    component:CrearUsuarioComponent
+  },
+  {
+    path: 'ERROR404',
+    component:E404Component
+  },
+  {
+    path: '**',
+    redirectTo: 'ERROR404',
+    pathMatch:'full'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//Componentes del Proyecto Seguricom
+import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
+import { CrearUsuarioComponent } from './components/crear-usuario/crear-usuario.component';
+import { E404Component } from './components/e404/e404.component';
+import { PrincipalComponent } from './components/principal/principal.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IniciarSesionComponent,
+    CrearUsuarioComponent,
+    E404Component,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,
