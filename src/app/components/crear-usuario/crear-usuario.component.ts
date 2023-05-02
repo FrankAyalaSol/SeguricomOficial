a501d1a30f7d89896ef50f4f17922253ddf6dcc9
FrankAyalaSol <201810595@urp.edu.pe>
2023-05-02 11:48:38 -0500

    V.1.8

--- src/app/components/crear-usuario/crear-usuario.component.ts
+++ src/app/components/crear-usuario/crear-usuario.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crear-usuario.component.css']
 })
 export class CrearUsuarioComponent implements OnInit {
-
+  user = {
+    firstname: '',
+    lastname: '',
+    email: '',
+    phone: '',
+    document_type: '',
+    document_number: '',
+    birth_date: '',
+    password: ''
+  }
   constructor() { }
 
   ngOnInit(): void {
