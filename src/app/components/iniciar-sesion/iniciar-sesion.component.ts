6fa1fe43806dbf55f98bf246f07f90cee86dc7c6
FrankAyalaSol <201810595@urp.edu.pe>
2023-05-03 11:47:40 -0500

    Inicio-Sesion,Registro sin validaciones
    Las validaciones son necesarias y el routing basado en roles, pendiente!

--- src/app/components/iniciar-sesion/iniciar-sesion.component.ts
+++ src/app/components/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-iniciar-sesion',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IniciarSesionComponent implements OnInit {
 
-  constructor() { }
+  user = {
+    email: '',
+    password: '',
+  }
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
   }
-
+  signIn() {
+    console.log(this.user);
+    this.authService.signInUser(this.user)
+      .subscribe(
+        res => {
+          console.log(res)
+          localStorage.setItem('token', res.token);
+          //this.router.navigate(['/pagina-cliente']);
+        },
+        err => console.log(err)
+      )
+  }
 }
