9d82ee0c213c93d204a253324d23b19011e15478
FrankAyalaSol <201810595@urp.edu.pe>
2023-06-07 10:54:19 -0500

    Solicitar Cita FRONT
    FRONT

--- src/app/components/solicitar-cita/solicitar-cita.component.ts
+++ src/app/components/solicitar-cita/solicitar-cita.component.ts
@@ -1,19 +1,56 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup,FormControl,FormArray,Validators} from '@angular/forms'
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators  } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service'
+import { Router } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-solicitar-cita',
   templateUrl: './solicitar-cita.component.html',
-  styleUrls: ['./solicitar-cita.component.css']
+  styleUrls: ['./solicitar-cita.component.css'],
 })
-export class SolicitarCitaComponent implements OnInit {
 
+export class SolicitarCitaComponent implements OnInit {
+  isFieldsBlocked: boolean = false;
+  [x: string]: any;
+  visitaForm: FormGroup;
+
+  visita={
+    tipos: '',
+    pregunta1: '',
+    pregunta2: '',
+    pregunta3: '',
+    pregunta4: '',
+    pregunta5: '',
+    pregunta6: ''
+  }
+
+  tipo_cliente = [
+    {tipo: "Casa"},
+    {tipo: "Empresa"}
+  ]
   public formParent: FormGroup = new FormGroup({});
 
-  constructor() { }
+  constructor(private fb: FormBuilder,  private authService: AuthService) {this.visitaForm = this.fb.group({
+    tipos: ['', Validators.required],
+    pregunta1: ['', Validators.required],
+    pregunta2: ['', Validators.required],
+    pregunta3: ['', Validators.required],
+    pregunta4: ['', Validators.required],
+    pregunta5: ['', Validators.required],
+    pregunta6: ['', Validators.required],
+  })
+ }
 
   ngOnInit(): void {
     this.initFormParent()
+
+  }
+  agregarVisita(){
+
+    console.log(this.visita);
+
   }
 
   initFormParent():void{
