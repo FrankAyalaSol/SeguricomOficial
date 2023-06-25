d272f7d46f255833ba450976f2ea7d47bfea95ae
FrankAyalaSol <201810595@urp.edu.pe>
2023-06-25 13:31:44 -0500

    Realizando Gerente

--- src/app/components/asignar-especialista/asignar-especialista.component.ts
+++ src/app/components/asignar-especialista/asignar-especialista.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Usuario } from 'src/app/models/usuario.model';
+import { AsignateSpecialistServiceService } from 'src/app/services/asignate-specialist.service.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-asignar-especialista',
@@ -7,9 +11,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AsignarEspecialistaComponent implements OnInit {
 
-  constructor() { }
+  id: string | null;
+  user = <Usuario>{};
 
+  constructor(public authService: AuthService, private asignate_specialist:AsignateSpecialistServiceService,private aRoute:ActivatedRoute, private router: Router) {
+    this.id = this.aRoute.snapshot.paramMap.get('id')
+   }
   ngOnInit(): void {
+  }
+
+  // obtenerClientesPendientes(){
+  //   if(this.id!==null){
+  //     console.log(this.id);
+  //     if(this.user.rol==4){
+  //       this.asignate_specialist.GetClient(this.user._id).subscribe(
+  //         res =>{
+  //     }
+
+  //         res.id
+  //         this.asignate_specialist.getClientesPendiente(this.id).subscribe(data=>{
+  //       }
+  //     )
+  //   }
+
+
+  //   },error =>{
+  //     console.log(error);
+  //   })
+  // }
+
+  // agregarVisita(){
+  //   if(this.id!==null){
+  //     console.log(this.visita);
+  //     this.order_service.GetClient(this.id).subscribe(
+  //       res =>{
+  //         const idCliente = res._id;
+  //         this.order_service.RegistrarDetalleEstudio(idCliente,this.visita).subscribe(
+  //           res => {
+  //             console.log(res);
+  //             this.router.navigate(['/Pagina_Cliente',res._id]);
+  //           },
+  //           err => {console.log(err)}
+  //         )
+  //       }
+  //     );
+
+  //   }
+
+  // }
+
+  volver() {
+    if (this.id !== null) {
+      this.asignate_specialist.GetClient(this.id).subscribe(
+        data => {
+          this.router.navigate(['/Pagina_Gerente', data._id])
+        },
+        error => { console.log(error) }
+      )
+    }
   }
 
 }
--- src/app/services/asignate-specialist.service.service.ts
+++ src/app/services/asignate-specialist.service.service.ts
@@ -8,11 +8,15 @@ import { Observable } from 'rxjs';
 })
 export class AsignateSpecialistServiceService {
   private URLID = 'http://localhost:3000/api/cliente/'
+  private ID = 'http://localhost:3000/api/gerente/'
 
   constructor(private http: HttpClient, private router: Router) { }
 
 
   GetClient(_id: string): Observable<any> {
     return this.http.get(this.URLID + _id);
+  }
+  getClientesPendiente(_idCliente:string,_idEspecialista:string):Observable<any>{
+    return this.http.get(this.ID+"clientes/"+_idCliente+"/detalleestudio/"+_idEspecialista);
   }
 }
